255c2c1a8623f2fa4e354d1eb59f82c85ff9a846
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-10-18 13:55:08 +0100

    completed exercise002 task 1

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,26 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+// const fetchData = (apiEndPoint) => {
+//   fetch(apiEndPoint)
+//     .then((response) => response.json())
+//     .then((json) => console.log(json))
+//     .catch((error) => console.log(error));
+// };
+
+// fetchData(jsonTypicode);
+
+const fetchData = async (apiEndPoint) => {
+
+  try {
+    const response = await fetch(apiEndPoint);
+    const result = await response.text();
+    console.log("result :", result)
+  }catch (err) {
+    console.log("error :", err)
+  }
+
+
 };
 
 fetchData(jsonTypicode);
