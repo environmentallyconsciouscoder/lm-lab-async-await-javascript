3c27b1b599925ff8f6738ff1a77a513045733bf5
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-10-18 13:34:20 +0100

    completed exercise001 task 1

--- exercise001.js
+++ exercise001.js
@@ -1,14 +1,23 @@
-const promise = new Promise((resolve, reject) => {
-	// Generate random number between 0 and 9
-	const randomInt = Math.floor(Math.random() * 10);
 
-	if (randomInt % 2 === 0) {
-		// Success
-		setTimeout(() => resolve('done'), 2000);
-	} else {
-		// Failure
-		setTimeout(() => reject('error'), 2000);
-	}
-});
+const fetchData = (randomInt) => {
+	return new Promise((resolve, reject) => {
+		// Generate random number between 0 and 9
+		// const randomInt = Math.floor(Math.random() * 10);
+
+		if (randomInt % 2 === 0) {
+			// Success
+			setTimeout(() => resolve('done'), 2000);
+		} else {
+			// Failure
+			setTimeout(() => reject('error'), 2000);
+		}
+	});
+}
+
 
 // Your solution(s) to exercise001 here!
+fetchData(2).then((response) => {
+	console.log(`Yay! Promise resolved with response: ${response}`)
+}).catch((err) => {
+	console.log(`Boo. Promise rejected with response:${err}`)
+})
